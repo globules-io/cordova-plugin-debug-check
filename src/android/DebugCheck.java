dc7bb2b3d3596cc303d11dc573f51da7975a1f2b
Eric <info@ericparlier.com>
2026-02-02 13:50:30 +0100

    Connected or Enabled debug

--- src/android/DebugCheck.java
+++ src/android/DebugCheck.java
@@ -3,17 +3,20 @@ package io.globules;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
-import org.json.JSONException;
 import android.os.Debug;
+import android.webkit.WebView;
 
 public class DebugCheck extends CordovaPlugin {
-     @Override
-     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
-          if ("isDebugging".equals(action)) {
-               boolean isDebugging = Debug.isDebuggerConnected();
-               callbackContext.success(isDebugging ? 1 : 0);
-               return true;
-          }
-          return false;
-     }
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+        if ("isDebugging".equals(action)) {
+            boolean javaDebugger = Debug.isDebuggerConnected();
+            boolean webviewDebugger = WebView.isDebuggingEnabled();
+            boolean isDebugging = javaDebugger || webviewDebugger;
+            callbackContext.success(isDebugging ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
 }
